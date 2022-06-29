56399eeae11007c427246c1a528859d1a2df7bab
BlackErik <blac.erik@gmail.com>
2022-06-29 11:41:39 -0600

    post session working

--- app.js
+++ app.js
@@ -18,14 +18,58 @@ var app = new Vue({
         // method: "GET",
         credentials: "include",
       });
-      console.log(response);
+      // Are we logged in?
+      if (response.status == 200) {
+        // logged in
+        console.log("logged in ");
+        let data = await response.json();
+        console.log(data);
+      } else if (response.status == 401) {
+        // not logged in
+        console.log("Not logged in");
+        let data = await response.json();
+        console.log(data);
+      } else {
+        console.log("Some Error in GET /session", response.status, response);
+      }
     },
 
     // POST /session - Attempt to Log in
     postSession: async function () {
+      let loginCredentials = {
+        username: this.loginEmailInput,
+        password: this.loginPasswordInput,
+      };
+
       let response = await fetch(`${API_URL}/session`, {
         method: "POST",
+        body: JSON.stringify(loginCredentials),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
       });
+
+      // parse response body
+      let body = response.json();
+      console.log(body);
+
+      // was the login successful
+      if (response.status == 201) {
+        console.log("Successful login attempt");
+
+        this.loginEmailInput = "";
+        this.loginPasswordInput = "";
+
+        // take user to a home page
+      } else if (response.status == 401) {
+        console.log("Unsuccessful login attempt");
+        alert("Unsuccessful login");
+
+        this.loginPasswordInput = "";
+      } else {
+        console.log("Some error in POST /session", response.status, response);
+      }
     },
 
     // POST /user - create a user
