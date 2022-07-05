c106fd75e1e084ea4d9ef6222a4bf28fe3f01422
BlackErik <blac.erik@gmail.com>
2022-07-05 09:22:55 -0600

    npm init, starting server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
